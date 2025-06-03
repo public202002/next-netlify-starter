73ddcf806edaac67c9f93a999bdf41f87a95cf8d
public202002 <77833368+public202002@users.noreply.github.com>
2025-06-03 20:16:57 +0200

    Update hola.js

--- public/hola.js
+++ public/hola.js
@@ -1,1 +1,8 @@
 alert("hola")
+
+fetch('/.netlify/functions/saveText', {
+    method: 'POST',
+    body: JSON.stringify({ text: "Toto je můj text" }),
+})
+.then(response => response.json())
+.then(data => console.log(data));
